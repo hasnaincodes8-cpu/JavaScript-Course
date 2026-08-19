94e88128d720606136cb0cb3a71248a35f973fbe
hasnaincodes8-cpu <hasnaincodes.8@gmail.com>
2026-08-19 16:45:02 +0000

    JavaScript 1

---
+++ Basics/first.js
@@ -0,0 +1,18 @@
+let value = 0
+
+let convert = Boolean(value)
+
+
+value = null
+
+convert = String(value)
+console.log(typeof convert);
+
+console.log(convert)
+
+let numberValue = 42
+
+let stringValue = String(numberValue)
+console.log(typeof stringValue);
+
+console.log(stringValue)
